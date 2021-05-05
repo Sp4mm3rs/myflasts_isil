fd50e57b6ae4b2e612ba3b28c63163c180870e7f
@Julio <jpillaca@atypax.com>
2021-05-05 18:32:18 -0500

    Cambio de texto conection.php

--- config/conection.php
+++ config/conection.php
@@ -2,7 +2,9 @@
 $mysqli = new mysqli("localhost", "root", "", "myflats");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}else{
+	echo "Conexion exitosa";
 }
-echo $mysqli->host_info . "\n";
+// echo $mysqli->host_info . "\n";
 
 ?>
