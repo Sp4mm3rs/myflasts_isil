b92043e6077fa893846ec350a0d9e4175c67092a
@Julio <jpillaca@atypax.com>
2021-05-06 18:01:34 -0500

    Actualizando html Ingresar inquilino

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,17 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable(
+
+  	 {
+        "language": {
+            "lengthMenu": "Display _MENU_ records per aaaa",
+            "zeroRecords": "Nothing found - sorry",
+            "info": "Showing page _PAGE_ of _PAGES_",
+            "infoEmpty": "No records available",
+            "infoFiltered": "(filtered from _MAX_ total records)"
+        }
+    }
+
+
+  	);
 });
