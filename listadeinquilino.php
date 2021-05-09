0968cdbdddfb47f11f3244366062e95099e9bec3
mjzarate <mjzarate06@gmail.com>
2021-05-08 19:37:50 -0500

    nav inquilinos

--- listadeinquilino.php
+++ listadeinquilino.php
@@ -70,13 +70,13 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Inquilinos</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                        <a class="collapse-item" href="listadeinquilino.php">Lista de inquilinos</a>
+                        <a class="collapse-item" href="ingresar-inquilino.php">Ingresar inquilino</a>
                     </div>
                 </div>
             </li>
