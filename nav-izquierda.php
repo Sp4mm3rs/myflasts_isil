9a8cb634550e782a15ea230f839057af78e870fa
mjzarate <mjzarate06@gmail.com>
2021-05-13 17:54:56 -0500

    nav

--- nav-izquierda.php
+++ nav-izquierda.php
@@ -53,7 +53,7 @@
                 </div>
             </li>
 
-            <li class="nav-item">
+            <!-- <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#menu-habitacion"
                     aria-expanded="true" aria-controls="menu-habitacion">
                     
@@ -61,9 +61,9 @@
                 </a>
                 <div id="menu-habitacion" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <!-- <h6 class="collapse-header">Custom Components:</h6> -->
+                        <h6 class="collapse-header">Custom Components:</h6>
                         <a class="collapse-item" href="listadehabitaciones.php">Lista de Habitaciones</a>
-                        <!--<a class="collapse-item" href="ingresar-habitacion.php">Ingresar Habitacion</a>-->
+                        <a class="collapse-item" href="ingresar-habitacion.php">Ingresar Habitacion</a>
                     </div>
                 </div>
             </li>
@@ -75,12 +75,12 @@
                 </a>
                 <div id="menu-servicio" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <!-- <h6 class="collapse-header">Custom Components:</h6> -->
+                        <h6 class="collapse-header">Custom Components:</h6>
                         <a class="collapse-item" href="#">Lista de Servicios</a>
                         <a class="collapse-item" href="#">Ingresar Servicio</a>
                     </div>
                 </div>
-            </li>
+            </li> -->
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <!-- <li class="nav-item">
