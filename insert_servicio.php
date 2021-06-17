0613212ef6a33cbb0b645ab50644e0b6a87b34cd
Julio Pillaca <pt.juliocesar@gmail.com>
2021-06-17 00:14:04 -0500

    Actualizando

--- insert_servicio.php
+++ insert_servicio.php
@@ -1,31 +1,62 @@
 <?php
     include 'config/conection.php';
 
-    $get_type = $_POST['serv_type'];
-    $get_fec = date('Y-m-d',strtotime($_POST['serv_fec']));
-    $get_precio = $_POST['serv_monto'];
+    $tipo = $_POST['serv_type'];
+    $fecha = date('Y-m-d',strtotime($_POST['serv_fec']));
+    $monto = $_POST['serv_monto'];
 
+    $mes = date('m',strtotime($_POST['serv_fec']));
+    $ano = date('Y',strtotime($_POST['serv_fec']));
 
-    $fecha_bd = "SELECT * FROM servicios";
-    $sql = "INSERT INTO servicios (tipo_servicio, fec_vencimiento, monto) VALUES ('$get_type', '$get_fec', $get_precio)";
+    $sql_ano = "SELECT YEAR(fec_vencimiento) AS mes_servicio FROM servicios WHERE YEAR(fec_vencimiento) = '$ano'";
 
+    $res_ano = mysqli_query($conexion, $sql_ano);
 
-   $res = mysqli_query($conexion, $fecha_bd );
+    if(!empty($res_ano->num_rows)) {
+            echo "<p class='avisos'>existe</p>";
+        $sql_mes = "SELECT MONTH(fec_vencimiento) AS mes_servicio FROM servicios WHERE MONTH(fec_vencimiento) = '$mes'";
+        $res_mes = mysqli_query($conexion, $sql_mes);
+        if(!empty($res_mes->num_rows)) {
+            echo "<p class='avisos'>existe</p>";
+            $sql_tipo = "SELECT tipo_servicio FROM servicios WHERE tipo_servicio = '$tipo'";
+                $res_tipo = mysqli_query($conexion, $sql_tipo);
 
-   while($row = mysqli_fetch_array($res)) {
-      
-    if($row["fec_vencimiento"] != $get_fec){
-      $resultado = mysqli_query($conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");   
+                if(!empty($res_tipo->num_rows)) {
+                         echo "<p class='avisos'>Ya esta registrado</p>";
+                    } else{
+                        $sql = "INSERT INTO servicios (tipo_servicio, fec_vencimiento, monto) VALUES ('$tipo', '$fecha', $monto)";
+                        $resultado = mysqli_query($conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");  
+                        if ($resultado) {
+                            header( 'Location: http://localhost/myflasts_isil/detalle-servicio.php' ) ;
+                        }else{
+                            echo "No ha sido registrado";
+                        }
+                        mysql_close($conexion);
+                    }
+        }else{
+            $sql = "INSERT INTO servicios (tipo_servicio, fec_vencimiento, monto) VALUES ('$tipo', '$fecha', $monto)";
+            $resultado = mysqli_query($conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");  
+            if ($resultado) {
+                header( 'Location: http://localhost/myflasts_isil/detalle-servicio.php' ) ;
+            }else{
+                echo "No ha sido registrado";
+            }
+            mysql_close($conexion);
+        }
 
-      if ($resultado) {
-         header( 'Location: http://localhost/myflasts_isil/detalle-servicio.php' ) ;
-      }
-
-      mysql_close($conexion); 
+    }else{
+        $sql = "INSERT INTO servicios (tipo_servicio, fec_vencimiento, monto) VALUES ('$tipo', '$fecha', $monto)";
+        $resultado = mysqli_query($conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");  
+        if ($resultado) {
+            header( 'Location: http://localhost/myflasts_isil/detalle-servicio.php' ) ;
+        }else{
+            echo "No ha sido registrado";
+        }
+        mysql_close($conexion);
     }
-}
-
     
 
-    
+   
+        
 
+ 
