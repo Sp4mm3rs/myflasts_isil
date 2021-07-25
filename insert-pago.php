37cb8433047df53c78e3c3c5104effa66f18528c
Chuskarin <ftejadaa@gmail.com>
2021-07-25 17:50:02 -0500

    insetpaogfix

--- insert-pago.php
+++ insert-pago.php
@@ -23,7 +23,14 @@ $monto = $_POST['monto_pago'];
     }
 
 
-    if($detalle=="Deposito"){
+    if($detalle=="Contado"){
+        $request2 = "INSERT INTO historial_pagos (dni,fechav,fechap,montopagado,observaciones) VALUES ($dni,'$fechav','$fechap',$monto,'$detalle')";
+        $slq2= mysqli_query($conexion,$request2) or die ( "Algo ha ido mal en la consulta a la base de datos");
+        if ($slq2) {
+            header( 'Location: http://localhost/myflats_isil/listadeinquilinos.php' ) ;
+            }
+        }
+    else{
         $request = "INSERT INTO historial_pagos (foto,dni,fechav,fechap,montopagado,observaciones) VALUES ('$folder',$dni,'$fechav','$fechap',$monto,'$detalle')";
         $slq= mysqli_query($conexion,$request) or die ( "Algo ha ido mal en la consulta a la base de datos");
         if ($slq) {
@@ -31,12 +38,6 @@ $monto = $_POST['monto_pago'];
         }
     }
    
-    if($detalle=="Contado"){
-    $request2 = "INSERT INTO historial_pagos (dni,fechav,fechap,montopagado,observaciones) VALUES ($dni,'$fechav','$fechap',$monto,'$detalle')";
-    $slq2= mysqli_query($conexion,$request2) or die ( "Algo ha ido mal en la consulta a la base de datos");
-    if ($slq2) {
-        header( 'Location: http://localhost/myflats_isil/listadeinquilinos.php' ) ;
-        }
-    }
+    
 
 }
